8ad6360f97725221d0b8bfc8237688e8e86dc237
jpgom3z <jpgom29@gmail.com>
2024-02-28 21:13:33 -0600

    Adding ChoreValidator that will be used on POST and PUT actions in the CRUD

---
+++ ToDoAPI/API/Validators/ChoreValidator/IChoreValidator.cs
@@ -0,0 +1,9 @@
+﻿using API.DataTransferObjects;
+
+namespace API.Validators
+{
+    public interface IChoreValidator
+    {
+        bool ValidateInsertUpdate(int? id, InsertUpdateChoreDTO data, List<string> messages);
+    }
+}
